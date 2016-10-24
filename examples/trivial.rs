eb326fd6d04001d3bec99c746c2cdfde3efac16d
Ceri Storey <cez@necrofish.org.uk>
2016-10-24 22:09:53 +0100

    Actually remove dep on tokio_timer fully.

--- examples/trivial.rs
+++ examples/trivial.rs
@@ -1,21 +1,16 @@
 extern crate fringe;
 extern crate futures;
-extern crate tokio_timer;
 extern crate fringe_futures;
 
 use futures::{Future };
 use fringe_futures::FringeFut;
-use tokio_timer::*;
 use std::time::Duration;
 
 fn main() {
-    let timer = Timer::default();
-
     let f = FringeFut::<usize, ()>::new(|yielder| {
         for n in 0..10 {
-            let dur = Duration::from_millis(100) * n;
-            println!("n:{:?}", dur);
-            yielder.await(timer.sleep(dur)).expect("await");
+            let r = yielder.await(futures::finished::<u32, ()>(n)).expect("await");
+            println!("n:{:?}", r);
         }
         Ok(42usize)
     });
